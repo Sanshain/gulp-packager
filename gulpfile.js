b6e6ae0997d766c52151c8ceeec92b41173a27ef
Alexandr <digital-mag@ya.ru>
2020-07-23 09:03:44 +0300

    relative imports

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@ var integrate = require('./pack')
 var ts = require('gulp-typescript');
 var pack = require('./index')
 
-gulp.task('default', function() {
+gulp.task('build', function() {
     
     return gulp.src('./samples/prime.ts') 
         .pipe(pack())
         .pipe(ts())
-        .pipe(gulp.dest('./samples'))    
+        .pipe(gulp.dest('./samples'))
+});
+
+gulp.task('default', function() {
+    gulp.watch('samples/**/init.ts', gulp.series('build')); 
 });
